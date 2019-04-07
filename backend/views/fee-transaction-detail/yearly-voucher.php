e6133a04b20279b0c19d5924accf1e9603da97f3
Nadia Gull <nadiagull285@gmail.com>
2019-04-07 13:27:44 +0500

    perform account transactions and remove bugs

--- backend/views/fee-transaction-detail/yearly-voucher.php
+++ backend/views/fee-transaction-detail/yearly-voucher.php
@@ -74,38 +74,118 @@
         if($_POST["reg_no"]){
             $regNo = $_POST["reg_no"];
             $stdId = Yii::$app->db->createCommand("SELECT std_id FROM std_personal_info WHERE std_reg_no= '$regNo'")->queryAll();
-            $std_id = $stdId[0]['std_id'];
-            $headId = Yii::$app->db->createCommand("SELECT std_enroll_head_id FROM std_enrollment_head INNER JOIN std_enrollment_detail ON std_enroll_head_id = std_enroll_detail_head_id WHERE std_reg_no= '$regNo'")->queryAll();
-            $classId = $headId[0]['std_enroll_head_id'];
+            if(empty($stdId)){
+            Yii::$app->session->setFlash('warning', "This registration number not exist! Select valid registration no.");
+            } else {
+                $std_id = $stdId[0]['std_id'];
+                $headId = Yii::$app->db->createCommand("SELECT std_enroll_head_id FROM std_enrollment_head INNER JOIN std_enrollment_detail ON std_enroll_head_id = std_enroll_detail_head_id WHERE std_reg_no= '$regNo'")->queryAll();
+                if(empty($headId)){
+                    Yii::$app->session->setFlash('warning', "This student is not enrolled in class.");
+                } else {
+                $class_id = $headId[0]['std_enroll_head_id'];
+                $sessionDetail = Yii::$app->db->createCommand("SELECT s.session_start_date, s.session_end_date FROM std_sessions as s INNER JOIN std_enrollment_head  as e ON s.session_id = e.session_id WHERE e.std_enroll_head_id = '$class_id'")->queryAll(); 
             
-        } else {
-            $classId = $_POST["classid"];
-            $std_id = $_POST["std"];
-            $regsNo = Yii::$app->db->createCommand("SELECT std_reg_no FROM std_personal_info WHERE std_id = '$std_id'")->queryAll();
-            $regNo = $regsNo[0]['std_reg_no'];
+                $yearStart = date('Y-m', strtotime($sessionDetail[0]['session_start_date']));
+                $yearEnd = date('Y-m', strtotime($sessionDetail[0]['session_end_date']));
+
+                $transactionHead = Yii::$app->db->createCommand("SELECT * FROM fee_transaction_head WHERE std_id = '$std_id' AND month >= '$yearStart' AND month <=  '$yearEnd'")->queryAll();
+                if(empty($transactionHead)){
+                     Yii::$app->session->setFlash('warning', "Plese manage the account first, for further proceedings.");
+                } else { 
+                global $count;
+                $count = count($transactionHead);
+
+                $studentID = $transactionHead[0]['std_id'];
+                $classID = $transactionHead[0]['class_name_id'];
+                
+                $student = Yii::$app->db->createCommand("SELECT std_name FROM std_personal_info WHERE std_id = '$studentID'")->queryAll();        
+
+                $class = Yii::$app->db->createCommand("SELECT class_name FROM std_class_name WHERE class_name_id = '$classID'")->queryAll(); ?>
+                <!-- modified collect voucher start -->
+<div class="row container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <h3 class="well well-sm">
+                <?php echo $regNo."   ".$student[0]['std_name']." - ".$class[0]['class_name']."<span style='float: right;'>"."Year - ".date('Y')."</span>"; ?>
+            </h3>
+        </div>
+    </div>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Month</th>
+                <th>Total Amount</th>
+                <th>Amount Paid</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php for ($i=0; $i < $count ; $i++) { 
+                if($transactionHead[$i]['status'] == 'Paid'){ ?>
+                    <tr>
+                        <td> <?php echo date('Y-F', strtotime($transactionHead[$i]['month'])); ?> </td>
+                        <td> <?php echo $transactionHead[$i]['total_amount']; ?> </td>
+                        <td> <?php echo $transactionHead[$i]['paid_amount']; ?> </td>
+                        <td> <?php echo $transactionHead[$i]['status']; ?> </td>
+                    </tr>
+            <?php } else {?>
+    <form method= "POST" action="">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <input type="hidden" name="_csrf" class="form-control" value="<?=Yii::$app->request->getCsrfToken()?>">          
+                            </div>    
+                        </div>    
+                    </div>
+                    <tr>
+                        <td> <?php echo date('Y-F', strtotime($transactionHead[$i]['month']));?> </td>
+                        <td> <?php echo $transactionHead[$i]['total_amount']; ?> </td>
+                        <td><input type="number" name="paidAmount[]" value="0" id="paidAmount_<?php echo $i; ?>" onchange="setStatus(<?php echo $i; ?>)"></td>
+                        <td><input type="text" name="status[]" value="Unpaid" id="status_<?php echo $i; ?>" readonly=""></td>
+                        <input type="hidden" name="voucherNo[]" value="<?php echo $transactionHead[$i]['fee_trans_id']; ?>">
+                            
+                    </tr>
+                    <?php } } ?>
+        </tbody>
+    </table>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <button type="submit" name="save" class="btn btn-success">Save</button>
+                </div> 
+            </div>
+            </div>  
+    </form>
+</div>
+<!-- modified collect voucher close --> 
+<?php
+                }
+                }
+            } 
         }
-        if(empty($std_id)){
-            Yii::$app->session->setFlash('warning', "This registration number not exist! Select valid registration no");
-        } else {
-        
-        $sessionDetail = Yii::$app->db->createCommand("SELECT s.session_start_date, s.session_end_date FROM std_sessions as s INNER JOIN std_enrollment_head  as e ON s.session_id = e.session_id WHERE e.std_enroll_head_id = '$classId'")->queryAll(); 
-        
-        $yearStart = date('Y-m', strtotime($sessionDetail[0]['session_start_date']));
-        $yearEnd = date('Y-m', strtotime($sessionDetail[0]['session_end_date']));
+        if($_POST["classid"] && $_POST["std"]) {
+            $classId = $_POST["classid"];
+            $stdId = $_POST["std"];
+            $regsNo = Yii::$app->db->createCommand("SELECT std_reg_no FROM std_personal_info WHERE std_id = '$stdId'")->queryAll();
+            $regNo = $regsNo[0]['std_reg_no'];
+            $sessionDetail = Yii::$app->db->createCommand("SELECT s.session_start_date, s.session_end_date FROM std_sessions as s INNER JOIN std_enrollment_head  as e ON s.session_id = e.session_id WHERE e.std_enroll_head_id = '$classId'")->queryAll(); 
+            
+            $yearStart = date('Y-m', strtotime($sessionDetail[0]['session_start_date']));
+            $yearEnd = date('Y-m', strtotime($sessionDetail[0]['session_end_date']));
 
-    	$transactionHead = Yii::$app->db->createCommand("SELECT * FROM fee_transaction_head WHERE std_id = '$std_id' AND month >= '$yearStart' AND month <=  '$yearEnd'")->queryAll();
-        global $count;
-        $count = count($transactionHead);
+            $transactionHead = Yii::$app->db->createCommand("SELECT * FROM fee_transaction_head WHERE std_id = '$stdId' AND month >= '$yearStart' AND month <=  '$yearEnd'")->queryAll();
+            if(empty($transactionHead)){
+                 Yii::$app->session->setFlash('warning', "Please manage the account first, for further proceedings.");
+            } else { 
+            global $count;
+            $count = count($transactionHead);
 
-        $studentID = $transactionHead[0]['std_id'];
-        $classID = $transactionHead[0]['class_name_id'];
-        
-        $student = Yii::$app->db->createCommand("SELECT std_name FROM std_personal_info WHERE std_id = '$studentID'")->queryAll();        
+            $studentID = $transactionHead[0]['std_id'];
+            $classID = $transactionHead[0]['class_name_id'];
+            
+            $student = Yii::$app->db->createCommand("SELECT std_name FROM std_personal_info WHERE std_id = '$studentID'")->queryAll();        
 
-        $class = Yii::$app->db->createCommand("SELECT class_name FROM std_class_name WHERE class_name_id = '$classID'")->queryAll();     
-    ?>  
-
-
+            $class = Yii::$app->db->createCommand("SELECT class_name FROM std_class_name WHERE class_name_id = '$classID'")->queryAll(); ?>
 <!-- modified collect voucher start -->
 <div class="row container-fluid">
     <div class="row">
@@ -145,8 +225,8 @@
                     <tr>
                         <td> <?php echo date('Y-F', strtotime($transactionHead[$i]['month']));?> </td>
                         <td> <?php echo $transactionHead[$i]['total_amount']; ?> </td>
-                        <td><input type="number" name="paidAmount[]" value="0" id="paidAmount" onchange="setStatus()"></td>
-                        <td><input type="text" name="status[]" value="Unpaid" id="status" readonly=""></td>
+                        <td><input type="number" name="paidAmount[]" value="0" id="paidAmount_<?php echo $i; ?>" onchange="setStatus(<?php echo $i; ?>)"></td>
+                        <td><input type="text" name="status[]" value="Unpaid" id="status_<?php echo $i; ?>" readonly=""></td>
                         <input type="hidden" name="voucherNo[]" value="<?php echo $transactionHead[$i]['fee_trans_id']; ?>">
                             
                     </tr>
@@ -161,11 +241,11 @@
             </div>
             </div>  
     </form>
-
 </div>
-<!-- modified collect voucher close -->
+<!-- modified collect voucher close --> 
 <?php 
-    }
+            }
+        } 
 }
 // isset close.... 
 ?>
@@ -176,17 +256,27 @@ if(isset($_POST['save'])){
     $status           = $_POST["status"];
     $voucherNo           = $_POST["voucherNo"];
     $counts =count($paidAmount);
+    $totalAmount = 0;
 
     $transaction = \Yii::$app->db->beginTransaction();
     try {
         for($i=0;$i<$counts;$i++){
-
-            $updateTransactionHead = Yii::$app->db->createCommand()->update('fee_transaction_head', ['paid_amount'=> $paidAmount[$i],  'status' => $status[$i]], ['fee_trans_id' => $voucherNo[$i]])->execute();
-            if ($updateTransactionHead) {
-                $transaction->commit();
-                // success alert message...
+            $updateTransactionHead = Yii::$app->db->createCommand()->update('fee_transaction_head', ['paid_amount'=> $paidAmount[$i],  'status' => $status[$i], 'collection_date'=> new \yii\db\Expression('NOW()')], ['fee_trans_id' => $voucherNo[$i]])->execute();
+            $totalAmount += $paidAmount[$i]; 
+        }
+        $account = Yii::$app->db->createCommand()->insert('account_transactions', [
+                            'account_nature'=> 'Income',  
+                            'account_register_id' => 5,
+                            'date' => new \yii\db\Expression('NOW()'),
+                            'description' => $status[0],
+                            'total_amount' => $totalAmount,
+                            'created_at' => new \yii\db\Expression('NOW()'),
+                            'created_by' => Yii::$app->user->identity->id,
+                        ])->execute();
+        if ($account) {
+            $transaction->commit();
+            // success alert message...
             Yii::$app->session->setFlash('success', "Voucher paid Successfully...!"); 
-            }
         }
          
     } catch (Exception $e) {
@@ -201,11 +291,11 @@ if(isset($_POST['save'])){
 </html>
 
 <script type="text/javascript">
-   function setStatus(){
-        var paidAmount = parseInt(document.getElementById('paidAmount').value);
+   function setStatus(i){
+        var paidAmount = parseInt($('#paidAmount_'+i).val());
         if( paidAmount > 0 ) {
             paid = "Paid";
-            $('#status').val(paid);
+            $('#status_'+i).val(paid);
         }
     }
 </script>
