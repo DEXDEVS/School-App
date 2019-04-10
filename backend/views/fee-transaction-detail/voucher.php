6af7328fc36931c6d033ea5b4a4da39f42fbb23a
anasshafqat <anasshafqat01@gmail.com>
2019-04-11 00:37:23 +0500

    modified voucher

--- backend/views/fee-transaction-detail/voucher.php
+++ backend/views/fee-transaction-detail/voucher.php
@@ -20,8 +20,8 @@
         $dueDate   = $_POST["due_date"];
         // change the format of dates....
         $issueDate  = date('d-m-Y', strtotime($issueDate));
-        $dueDate  = date('d-m-Y', strtotime($dueDate));
-        $todayDate = date('d-m-Y'); 
+        $dueDate    = date('d-m-Y', strtotime($dueDate));
+        $todayDate  = date('d-m-Y'); 
         
         $months = Yii::$app->db->createCommand("SELECT month FROM fee_transaction_head WHERE month = '$month' AND class_name_id = '$classid' AND session_id = '$sessionid' AND section_id = '$sectionid'")->queryAll();
        
@@ -38,14 +38,14 @@
         // Select Students...
         $student = Yii::$app->db->createCommand("SELECT sed.std_enroll_detail_id ,sed.std_enroll_detail_std_id,sed.std_roll_no FROM std_enrollment_detail as sed INNER JOIN std_enrollment_head as seh ON seh.std_enroll_head_id = sed.std_enroll_detail_head_id WHERE seh.class_name_id = '$classid' AND seh.session_id = '$sessionid' AND seh.section_id = '$sectionid'")->queryAll();
         foreach ($student as $id =>$value) {
-			$stdInfo = Yii::$app->db->createCommand("SELECT std_name , std_father_name  FROM std_personal_info WHERE std_id = '$value[std_enroll_detail_std_id]'")->queryAll();
+			$stdInfo = Yii::$app->db->createCommand("SELECT std_name, std_father_name  FROM std_personal_info WHERE std_id = '$value[std_enroll_detail_std_id]'")->queryAll();
 			$stdId = $value['std_enroll_detail_std_id'];
 			$feeDetail = Yii::$app->db->createCommand("SELECT * FROM fee_transaction_detail as ftd INNER JOIN fee_transaction_head as fth ON fth.fee_trans_id = ftd.fee_trans_detail_head_id WHERE fth.std_id = '$stdId' AND fth.month = '$month'")->queryAll();
 			// if(!empty($feeDetail)){
 	
 			// 	$installmentId = $feeDetail[0]['installment_no'];
 			// 	$installment = Yii::$app->db->createCommand("SELECT installment_name FROM installment WHERE installment_id = '$installmentId'")->queryAll();
-			$feeMonth = date('F, Y', strtotime($month));
+			//$feeMonth = date('F, Y', strtotime($month));
 		
 			$feeType = Yii::$app->db->createCommand("SELECT fee_type_id,fee_type_name  FROM fee_type")->queryAll();
     	?>
@@ -53,16 +53,16 @@
 			<div class="container-fluid">
 				<div class="row">
 					<?php 
-					$j=4;
-					$copyName = Array('Student Copy','Account Copy','Bank Copy');
-					for ($i=0;$i<3;$i++){ ?>
+					$j=3;
+					$copyName = Array('Student Copy','Principal Copy','Account Copy','Bank Copy');
+					for ($i=0;$i<4;$i++){ ?>
 						<div class="col-md-<?php echo $j; ?>" style="border-right: black dashed 2px;">
 							<div class="row">
 								<div class="col-md-3">
 									<img src="images/abc_logo.jpg" class="img-circle img-responsive" style="float: left;" width="100px" >
 								</div>
 								<div class="col-md-9">
-									<h3 style="margin-left: 50px;">
+									<h3 style="margin-left: 20px;">
 										<?php echo $institue[0]['institute_name'] ?>
 									</h3>
 									<h4 style="margin-top: -5px; margin-left: 100px;">
@@ -142,29 +142,29 @@
 								<div class="col-md-12">
 									<table class="table table-bordered" width="100%">
 										<tr>
-											<th>Sr #</th>
-											<th colspan="2">Descrpition</th>
-											<th style="text-align: center;">Amount</th>
+											<th class="text-center"><i>Sr #</i></th>
+											<th colspan="2"><i>Descrpition</i></th>
+											<th style="text-align: center;"><i>Amount</i></th>
 										</tr>
 										<?php foreach ($feeType as $index => $value) { ?>
 										<tr>
-											<td align="center"><?php echo ($index +1);?></td>
-											<td colspan="2">
+											<th class="text-center"><i><?php echo ($index +1);?></i></th>
+											<td colspan="2"><i>
 												<?php if ($feeType[$index]['fee_type_name'] == 'Tuition Fee') {
 													echo $feeType[$index]['fee_type_name']. ' (' .date('F, Y', strtotime($months[0]["month"])).')';
 												}
 												else{
 												echo $feeType[$index]['fee_type_name'];
-												}?>		
+												}?></i>		
 											</td>
-											<td align="center">
+											<td align="center"><i><b>
 												<?php
 													foreach ($feeDetail as $key => $value) { 
 														if($feeDetail[$key]['fee_type_id'] == $feeType[$index]['fee_type_id'] ){
 															echo $feeDetail[$key]['fee_amount'];
 														}		
 													} 
-												?>
+												?></i></b>
 											</td>
 										</tr>
 										<?php } ?>	
@@ -174,10 +174,11 @@
 							<?php
 							$currentTotal  = $feeDetail[0]['total_amount'];
 							$feeMonth     = $feeDetail[0]['month'];
+							$lastMonth = date('Y-m', strtotime('-1 months', strtotime($feeMonth))); 
 							//$installmentNo = $feeMonth -1;
 							$remaining = 0;
 							$remainig= 0;
-							$remain = Yii::$app->db->createCommand("SELECT status ,remaining, total_amount, paid_amount FROM fee_transaction_head WHERE month = $feeMonth AND std_id = $stdId")->queryAll();
+							$remain = Yii::$app->db->createCommand("SELECT status, remaining, total_amount, paid_amount FROM fee_transaction_head WHERE month = '$lastMonth' AND std_id = '$stdId'")->queryAll();
 							if (empty($remain)) {
 								$remainig = 0;
 							} else if($remain[0]['status'] == 'Partially Paid') {
@@ -185,34 +186,26 @@
 							} else if($remain[0]['status'] == 'Unpaid'){
 								$remainig = $remain[0]['total_amount'];
 							}
-							$paymentByDueDate = $currentTotal + $remaining;
-							$paymentAFterDueDate = $paymentByDueDate + 50;
+							$paymentByDueDate = $currentTotal;
+							$paymentAFterDueDate = $paymentByDueDate + 100;
 							?>			
 							<div class="row">
 								<div class="col-md-12">
 									<table width="100%" class="table table-condensed">
 										<tr>
-											<th>Current Voucher Total:</th>
-											<td align="center"> <?php echo $currentTotal; ?> </td>
+											<th><i>Total Voucher Payment till Due Date:</i></th>
+											<th><i><?php echo $paymentByDueDate; ?></i></th>
 										</tr>
 										<tr>
-											<th>Arrears:</th>
-											<td align="center"> <?php echo $remainig; ?> </td>
-										</tr>
-										<tr>
-											<th>Total Paymeny by Due Date:</th>
-											<td align="center"><?php echo $paymentByDueDate; ?></td>
-										</tr>
-										<tr>
-											<th>Total Payment After Due Date:</th>
-											<td align="center"><?php echo $paymentAFterDueDate; ?></td>
+											<th><i>Total Voucher Payment after Due Date:</i></th>
+											<th><i><?php echo $paymentAFterDueDate; ?></i></th>
 										</tr>
 									</table>
 								</div>
 							</div>
 							<?php 
 								if($remainig > 0){
-									$message = "Your remaining dues are pending by Rs: ".$remainig."/-";
+									$message = "Your remaining dues are pending by Rs:".$remainig;
 								} else {
 									$message = '';
 								}
@@ -228,9 +221,9 @@
 								<div class="col-md-12">
 									<h5>PAYMENT TERMS:</h5>
 									<ol class="text-justify" style="font-size: 13px">
-										<li>Late payment surcharge @Rs. 50/- per day.</li>
+										<li>Late payment surcharge @Rs.100/-</li>
 										<li>Rs.50/- will be charged for each dublicate voucher.</li>
-										<li>Any error in calculation of dues by the college will be adjusted in the next voucher.</li>
+										<li>Any error in calculation of dues by the school will be adjusted in the next voucher.</li>
 									</ol>
 								</div>
 							</div>
