8fac14649fdea5297766a8ebd6ae58f03e1034b0
naumanshahid007 <hwhashmi1625@gmail.com>
2019-06-12 15:06:53 +0500

    marks sheet modify

--- frontend/views/marks-details/marks-sheet.php
+++ frontend/views/marks-details/marks-sheet.php
@@ -12,17 +12,17 @@ if(isset($_GET['class_id']))
             WHERE std_enroll_head_id = '$classID'")->queryAll();
     $classNameId = $ClassNameID[0]['class_name_id'];
 
-	$examDataCond = Yii::$app->db->createCommand("SELECT c.exam_criteria_id,c.exam_category_id,s.full_marks,s.passing_marks,c.exam_type, YEAR(c.exam_start_date)
-			FROM exams_criteria as c
-			INNER JOIN exams_schedule as s 
-			ON c.exam_criteria_id = s.exam_criteria_id
-			WHERE c.class_id = '$classNameId'  
-			AND c.exam_status = 'conducted' 
-			AND s.subject_id = '$subID'
-			AND (c.exam_type = 'Regular'
-			OR c.exam_type = 'Supply')
+	$examDataCond = Yii::$app->db->createCommand("SELECT c.exam_criteria_id,c.exam_category_id,s.full_marks,s.passing_marks,c.exam_type
+	FROM exams_criteria as c
+	INNER JOIN exams_schedule as s 
+	ON c.exam_criteria_id = s.exam_criteria_id
+	WHERE c.class_id = '$classNameId'  
+	AND c.exam_status = 'conducted' 
+	AND c.exam_type = 'Regular'
+	OR c.exam_type = 'Supply'
+	AND s.subject_id = '$subID'
 				")->queryAll();
-
+	//print_r($examDataCond);
 	if (empty($examDataCond)){
 		Yii::$app->session->setFlash('warning', "No Exam Found.!");
 	} else {
@@ -37,30 +37,38 @@ if(isset($_GET['class_id']))
 		AND s.subject_id = '$subID' 
 		AND s.status = 'result prepared'
 					")->queryAll();
-
 		if(!empty($examDataResult)){
 			Yii::$app->session->setFlash('warning', "You have already submitted mark sheet..!");
 		} else {
-			$examCatId = $examDataCond[0]['exam_category_id'];
+		$examCatId = $examDataCond[0]['exam_category_id'];
+		$examCatName = Yii::$app->db->createCommand("SELECT category_name
+		FROM exams_category
+		WHERE exam_category_id = '$examCatId' 
+					")->queryAll();
+		$subjectName = Yii::$app->db->createCommand("SELECT subject_name
+		FROM subjects
+		WHERE subject_id = '$subID' 
+					")->queryAll();
+		$empName = Yii::$app->db->createCommand("SELECT emp_name
+		FROM emp_info
+		WHERE emp_id = '$empID' 
+					")->queryAll();
+		$students = Yii::$app->db->createCommand("SELECT d.std_roll_no, d.std_enroll_detail_std_id, d.std_enroll_detail_std_name
+		FROM std_enrollment_detail as d 
+		INNER JOIN std_enrollment_head as h 
+		ON h.std_enroll_head_id = d.std_enroll_detail_head_id
+		WHERE h.std_enroll_head_id = '$classID'")->queryAll();
+		$countStudents = count($students);
 
-			$examCatName = Yii::$app->db->createCommand("SELECT category_name
-			FROM exams_category
-			WHERE exam_category_id = '$examCatId' 
-						")->queryAll();
-			$subjectName = Yii::$app->db->createCommand("SELECT subject_name
-			FROM subjects
-			WHERE subject_id = '$subID' 
-						")->queryAll();
-			$empName = Yii::$app->db->createCommand("SELECT emp_name
-			FROM emp_info
-			WHERE emp_id = '$empID' 
-						")->queryAll();
-			$students = Yii::$app->db->createCommand("SELECT d.std_roll_no, d.std_enroll_detail_std_id, d.std_enroll_detail_std_name
-			FROM std_enrollment_detail as d 
-			INNER JOIN std_enrollment_head as h 
-			ON h.std_enroll_head_id = d.std_enroll_detail_head_id
-			WHERE h.std_enroll_head_id = '$classID'")->queryAll();
-			$countStudents = count($students);
+		$marksWeightageDetails = Yii::$app->db->createCommand("SELECT d.weightage_type_id,d.marks
+		FROM marks_weightage_details as d 
+		INNER JOIN marks_weightage_head as h 
+		ON h.marks_weightage_id = d.weightage_head_id
+		WHERE h.class_id = '$classNameId'
+		AND h.subjects_id = '$subID'
+		AND h.exam_category_id = '$examCatId'")->queryAll();
+		print_r($marksWeightageDetails);
+
 
  ?>
 <!DOCTYPE html>
@@ -73,11 +81,12 @@ if(isset($_GET['class_id']))
 	<div class="row">
 		<div class="col-md-12">
 			<div class="panel panel-primary">
+				
 				<div class="panel-body">
 					<div class="row">
 						<div class="col-md-12">
 							<h2 style="text-align: center;font-family: georgia;box-shadow: 1px 1px 1px 1px;">
-							<?php echo $examCatName[0]['category_name']; ?> (<?php echo $examDataCond[0]['YEAR(c.exam_start_date)']; ?>)
+							<?php echo $examCatName[0]['category_name']; ?> (<?php echo date('Y'); ?>)
 							</h2>
 							<br>
 							<p style="text-align: center;font-weight: bold;font-size: 20px;">Marks Sheet:
@@ -118,7 +127,6 @@ if(isset($_GET['class_id']))
 					</div><hr>
 					<div class="row">
 						<div class="col-md-12">
-<<<<<<< HEAD
 							<form method="POST" action="./activity-view?sub_id=<?php echo $subID; ?>&class_id=<?php echo $classID; ?>&emp_id=<?php echo $empID; ?>">
 							<table class="table table-hover">
 								<thead>
@@ -126,83 +134,85 @@ if(isset($_GET['class_id']))
 										<th class="text-center">Sr#</th>
 										<th class="text-center">Roll no.</th>
 										<th class="text-center">Student</th>
-										<th class="text-center">Marks <?php echo $examDataCond[0]['full_marks']."/".$examDataCond[0]['passing_marks'] ?></th>
+										<th colspan="2" class="text-center">Marks <?php echo $examDataCond[0]['full_marks']."/".$examDataCond[0]['passing_marks'] ?></th>
 									</tr>
 								</thead>								<tbody>
 									<?php 
 									for ($j=0; $j <$countStudents ; $j++) { 			
 									?>
-									<tr align="center">
-										<td>
-											<?php echo $j+1; ?>	
-										</td>
-										<td>
-											<?php echo $students[$j]['std_roll_no']; ?>	
-										</td>
-										<td>
-											<?php echo $students[$j]['std_enroll_detail_std_name']; ?>
-										</td>
-										<td>
-											<input type="checkbox" name="marks<?php echo $j+1;?>" onclick=" remove(<?php echo $j+1; ?>)" value="A" id="check<?php echo $j+1; ?>">Abs <br>
-											<input type="text" name="marks<?php echo $j+1;?>" onkeypress="return (event.charCode == 8 || event.charCode == 0 || event.charCode == 13 || event.charCode == 65 || event.charCode == 46) ? null : event.charCode >= 48 && event.charCode <= 57" id="marks<?php echo $j+1;?>">
-										</td>
-=======
-							<form method="POST" action="./activity-view?sub_id=<?php echo $subID; ?>	&class_id=<?php echo $classID; ?>&emp_id=<?php echo $empID; ?>">
-								<table class="table table-hover">
-									<thead>
-										<tr style="background-color: #337AB7;color: white;">
-											<th class="text-center">Sr#</th>
-											<th class="text-center">Roll no.</th>
-											<th class="text-center">Student</th>
-											<th class="text-center">Marks <?php echo $examDataCond[0]['full_marks']."/".$examDataCond[0]['passing_marks'] ?></th>
-										</tr>
-									</thead>								
-									<tbody>
->>>>>>> a05e3ac640801b9220c9419fa649ab8d7011f5c7
-										<?php 
-										for ($j=0; $j <$countStudents ; $j++) { 			
-										?>
+									<div class="row">
 										<tr align="center">
-											<td><?php echo $j+1; ?></td>
-											<td><?php echo $students[$j]['std_roll_no']; ?></td>
-											<td>
-												<?php echo $students[$j]['std_enroll_detail_std_name']; ?>
-											</td>
-											<td>
-												<input type="checkbox" name="marks<?php echo $j+1;?>" onclick=" remove(<?php echo $j; ?>)" value="A" id="radio">Abs <br>
-												<input type="text" name="marks<?php echo $j+1;?>" onkeypress="return (event.charCode == 8 || event.charCode == 0 || event.charCode == 13 || event.charCode == 65 || event.charCode == 46) ? null : event.charCode >= 48 && event.charCode <= 57" id="marks<?php echo $j+1;?>">
-											</td>
-											<?php 
-											$stdID = $students[$j]['std_enroll_detail_std_id'];
-											$studentId[$j] = $stdID;
-											?>
-										</tr>
-										<?php }
-										//closing of for loop j ?>
-									</tbody>
-								</table>
-								<?php foreach ($studentId as $value) {
-					                		echo '<input type="hidden" name="stdId[]" value="'.$value.'" style="width: 30px">';
-					                	}
-					            ?>
-					            <input class="form-control" type="hidden" name="countStudents" value="<?php echo $countStudents; ?>">
-					            <input class="form-control" type="hidden" name="categoryId" value="<?php echo $examCatId; ?>">
-					            <input class="form-control" type="hidden" name="examCriteriaId" value="<?php echo $examCriteriaId; ?>">
-					            <input class="form-control" type="hidden" name="classHeadId" value="<?php echo $classID; ?>">
-					            <input class="form-control" type="hidden" name="subId" value="<?php echo $subID; ?>">
-					            <input class="form-control" type="hidden" name="classNameId" value="<?php echo $classNameId; ?>">
+											<div class="col-md-5">
+												<td>
+													<?php echo $j+1; ?>	
+												</td>
+												<td>
+													<?php echo $students[$j]['std_roll_no']; ?>	
+												</td>
+												<td>
+													<?php echo $students[$j]['std_enroll_detail_std_name']; ?>
+												</td>
+											</div>
+											<div class="col-md-3">
+												<td>
+													<?php $countWeightage = count($marksWeightageDetails); ?>
+													<input type="checkbox" name="marks<?php echo $j+1;?>" onclick=" remove(<?php echo $j+1 ?>,<?php echo $countWeightage; ?>)" value="A" id="check<?php echo $j+1; ?>">Abs
+												</td>
+											</div>
+											<div class="col-md-4">
+												<td>
+										<?php if(!empty($marksWeightageDetails)){
+											
+												for ($i=0; $i <$countWeightage ; $i++) {
+													$weightageTypeId = $marksWeightageDetails[$i]['weightage_type_id'];
 
-								<button style="float: right;s" type="submit" name="saveMarks" class="btn btn-success btn-flat btn-xs" onclick="return confirm('are you sure')">
+													$weightageName = Yii::$app->db->createCommand("SELECT weightage_type_name
+														FROM marks_weightage_type 
+														WHERE weightage_type_id = '$weightageTypeId'")->queryAll();
+											?> 
+											<div class="col-md-2">
+												<label><?php echo $weightageName[0]['weightage_type_name']; ?></label>	
+												<input class="form-control" type="text" name="marks<?php echo $i+1;?>" onkeypress="return (event.charCode == 8 || event.charCode == 0 || event.charCode == 13 || event.charCode == 65 || event.charCode == 46) ? null : event.charCode >= 48 && event.charCode <= 57" id="marks<?php echo $j+1;?>">
+											</div>	
+										<?php } //closing of for loop
+											} //closing of for loop
+											?>
+											
+										</td>
+											</div>
+										</tr>
+									</div>
+										<!-- row close -->
+										<?php 
+										$stdID = $students[$j]['std_enroll_detail_std_id'];
+										$studentId[$j] = $stdID;
+										?>
+									<?php }
+									//closing of for loop j ?>
+								</tbody>
+							</table>
+							<?php foreach ($studentId as $value) {
+				                		echo '<input type="hidden" name="stdId[]" value="'.$value.'" style="width: 30px">';
+				                	}
+				                	?>
+				             <input class="form-control" type="hidden" name="countStudents" value="<?php echo $countStudents; ?>">
+				             <input class="form-control" type="hidden" name="categoryId" value="<?php echo $examCatId; ?>">
+				             <input class="form-control" type="hidden" name="examCriteriaId" value="<?php echo $examCriteriaId; ?>">
+				             <input class="form-control" type="hidden" name="classHeadId" value="<?php echo $classID; ?>">
+				             <input class="form-control" type="hidden" name="subId" value="<?php echo $subID; ?>">
+				             <input class="form-control" type="hidden" name="classNameId" value="<?php echo $classNameId; ?>">
+
+							<button style="float: right;s" type="submit" name="saveMarks" class="btn btn-success btn-flat btn-xs" onclick="return confirm('are you sure')">
 									<i class="fa fa-sign-in"></i> <b>Submit Marks</b>
 								</button>
 							</form>
 						</div>
 					</div> <hr>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
 </body>
 </html>
 <?php //end of (result) else
@@ -211,18 +221,23 @@ if(isset($_GET['class_id']))
 	}
 //end of if isset
 } 
+
 ?>
 <script type="text/javascript">
-	function remove(i)
+	function remove(i,j)
 	{
+		alert(i);
+		alert(j);
 		
         if ($('#check'+i).prop('checked')) {
-        	//alert("checked");				
-	 			$('#marks'+i). prop("disabled", true);
-	 			$("#marks"+i).val("Absent");
-	 			j=i+1;
-	 			$("#marks"+j).focus();
-	 		}
+        	for (var a = 0; a <= j; a++) {
+        		//alert("checked");				
+	 			$('#marks'+a). prop("disabled", true);
+	 			$("#marks"+a).val("Absent");
+	 			j=a+1;
+	 			$("#marks"+j).focus();	
+        	}	
+	 	}
 	 		else{
 	 			//alert("unchecked");
 	 			$('#marks'+i). prop("disabled", false);
